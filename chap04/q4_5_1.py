20355880f270ebfa03c083acdec11b575446cf4b
itc-s26011 <s26011@std.it-college.ac.jp>
2026-07-21 03:14:31 +0000

    4.5 関数はオブジェクトやり直し

--- chap04/q4_5_1.py
+++ chap04/q4_5_1.py
@@ -2,3 +2,9 @@ functions = [sum, min, max]
 
 numbers = range(1, 11)
 print(list(numbers))
+
+functions = [sum, min, max]
+number_list = range(1, 11)
+for func in functions:
+    print("Function: {}, Result: {}".format(
+        func.__name__, func(number_list)))
