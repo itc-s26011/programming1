a99ed6dc24c046dfa55c1f3c441f9ca53f4564c0
itc-s26011 <s26011@std.it-college.ac.jp>
2026-07-22 01:15:02 +0000

    4.6 ラムダ式やり直し

--- chap04/q4_5_2.py
+++ chap04/q4_5_2.py
@@ -4,12 +4,13 @@ s_coordi_list = ["1.0,2.2,3.5", "2.1,3.2,5.5", "1.2,1.3,2.2",
 def str_to_float_coordi(s_coordi):
     '''与えられた文字列をカンマで分割しfloat型に変換して数値リストを返す'''
     p = s_coordi.split(",")
-    return list(map(float, p))
+    f_coordi = []
+    for n in p:
+        f_coordi.append(float(n))
+    return f_coordi
 
-def str_to_float_coordi_iter(s_coordi_list):
-    '''与えられたリストの要素を str_to_float_coordi() 関数で処理して結果を指す反復可能体(iterable)を返す'''
-    return map(str_to_float_coordi, s_coordi_list)
-
-f_coordi_list = list(str_to_float_coordi_iter(s_coordi_list))
+f_coordi_list = []
+for s_coordi in s_coordi_list:
+    f_coordi_list.append(str_to_float_coordi(s_coordi))
 
 print(f_coordi_list)
--- chap04/q4_6_1.py
+++ chap04/q4_6_1.py
@@ -1,9 +1,10 @@
-print(list(range(1, 8)))
+list(range(1, 8))
 
 "{:04}".format(5)
 
 str_num_list = map(lambda x: "{:04}".format(x), range(1, 8))
 str_num_list
+
 print(list(str_num_list))
 
 print(["{:0=4}".format(x) for x in range(1, 8)])
